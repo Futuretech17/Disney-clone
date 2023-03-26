c53195f14a4e49e941a6db9238e9eb4cc1a63810
Benjamin <benjamisaac7@gmail.com>
2023-03-26 19:09:15 +0300

    login page complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from '/Github/disney-clone/src/components/Login'
+import Login from './components/Login';
+
 
 
 function App() {
